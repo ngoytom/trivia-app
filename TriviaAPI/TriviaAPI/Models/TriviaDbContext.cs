3195f9a32bb2b02ea6c05e4c5c4816de529e7955
Tommy Ngoy <tommyngoy@outlook.com>
2022-10-04 17:45:48 -0400

    intial project commit

---
+++ TriviaAPI/TriviaAPI/Models/TriviaDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TriviaAPI.Models
+{
+    public class TriviaDbContext:DbContext
+    {
+        public TriviaDbContext(DbContextOptions<TriviaDbContext> options) : base(options)
+        { }
+
+        public DbSet<Question> Questions { get; set; }
+        public DbSet<Participant> Participants { get; set; }
+    }
+}
